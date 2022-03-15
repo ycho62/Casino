9d67cd1e5a5977ef2f86aa6e4eeeb0663e4cf809
Clyde Broderick <clyderick3@gmail.com>
2022-03-14 21:50:44 -0400

    Started SlotsGame.

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -1,7 +1,13 @@
 package com.github.zipcodewilmington.casino.games.slots;
 
+import java.util.Random;
+
 /**
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame {
+
+    public static void main(String []  args) {
+        Random random = new Random();
+    }
 }
