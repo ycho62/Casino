d1e4b6daf5b2af91358ec412ecb214bd85e7480c
Clyde Broderick <clyderick3@gmail.com>
2022-03-15 23:53:34 -0400

    Added randon number generator.

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -1,6 +1,7 @@
 package com.github.zipcodewilmington.casino.games.slots;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by leon on 7/21/2020.
@@ -9,5 +10,16 @@ public class SlotsGame {
 
     public static void main(String []  args) {
         Random random = new Random();
+        Scanner scanner = new Scanner (System.in);
+        int column1;
+        int column2;
+        int column3;
+
+        column1 = random.nextInt();
+        column2 = random.nextInt();
+        column3 = random.nextInt();
+
+        System.out.println("|"+column1+"|"+column2+"|"+column3+"|");
+
     }
 }
