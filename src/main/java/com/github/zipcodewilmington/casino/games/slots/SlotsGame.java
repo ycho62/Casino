fe6172ca52b61a3dfa73539c08ba2520928e2c53
Clyde Broderick <clyderick3@gmail.com>
2022-03-16 09:35:15 -0400

    Updated interface.

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -11,13 +11,15 @@ public class SlotsGame {
     public static void main(String []  args) {
         Random random = new Random();
         Scanner scanner = new Scanner (System.in);
+        int min = 0;
+        int max = 10;
         int column1;
         int column2;
         int column3;
 
-        column1 = random.nextInt();
-        column2 = random.nextInt();
-        column3 = random.nextInt();
+        column1 = random.nextInt(10);
+        column2 = random.nextInt(10);
+        column3 = random.nextInt(10);
 
         System.out.println("|"+column1+"|"+column2+"|"+column3+"|");
 
