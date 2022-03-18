682e81d7931ce7a87e0fb1e34fb05bb66694b8d9
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-18 16:17:02 -0400

    fixed get roll

--- src/main/java/com/github/zipcodewilmington/casino/games/GameInterface/GamblingGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/GameInterface/GamblingGame.java
@@ -1,11 +1,10 @@
 package com.github.zipcodewilmington.casino.games.GameInterface;
 
 import com.github.zipcodewilmington.casino.games.Person.Player;
-import com.github.zipcodewilmington.casino.games.ceelo.CeeLoPlayer;
+
 
 public interface GamblingGame <PlayerType extends Player> extends Games<PlayerType>{
     void setBets();
     void distributeWinningsToWinners(PlayerType winner);
 
-    void winConditionCheck(CeeLoPlayer player1, CeeLoPlayer player2);
 }
